8cb733b778f563abd3ecc6278044634d2305e33b
Dewantara <drawww111@gmail.com>
2025-09-28 00:58:00 +0900

    Feat : add. Section component

--- app/Filament/Resources/Mahasiswas/Schemas/MahasiswaForm.php
+++ app/Filament/Resources/Mahasiswas/Schemas/MahasiswaForm.php
@@ -2,6 +2,7 @@
 
 namespace App\Filament\Resources\Mahasiswas\Schemas;
 
+use Filament\Schemas\Components\Section;
 use Filament\Schemas\Schema;
 use Filament\Forms\Components\TextInput;
 
@@ -10,27 +11,27 @@ class MahasiswaForm
     public static function configure(Schema $schema): Schema
     {
         return $schema
-            ->components([
+            ->components(
+                Section::make('Form Data Mahasiswa')
+                    ->schema([
+                        TextInput::make('nama')
+                            ->placeholder("Masukan nama Mahasiswa")
+                            ->required(),
+
+                        TextInput::make('npm')
+                            ->placeholder("Masukan npm Mahasiswa")
+                            ->numeric()
+                            ->required(),
+
+                        TextInput::make('fakultas')
+                            ->placeholder("Masukan Fakultas Mahasiswa")
+                            ->required(),
+
+                        TextInput::make('program_studi')
+                            ->placeholder("Masukan Prodi Mahasiswa")
+                            ->required(),
+                    ])
                 //
-                TextInput::make('nama')
-                    ->placeholder("Masukan nama Mahasiswa")
-                    ->required(),
-
-                TextInput::make('npm')
-                    ->placeholder("Masukan npm Mahasiswa")
-                    ->numeric()
-                    ->required(),
-
-                TextInput::make('fakultas')
-                    ->placeholder("Masukan Fakultas Mahasiswa")
-                    ->required(),
-
-                TextInput::make('program_studi')
-                    ->placeholder("Masukan Prodi Mahasiswa")
-                    ->required(),
-
-
-
-            ]);
+            );
     }
 }
