6f91892f0fb81309db227696485285f7e822c325
Dewantara <drawww111@gmail.com>
2025-10-14 05:34:07 +0900

    Create logo.blade.php

---
+++ resources/views/filament/logo.blade.php
@@ -0,0 +1,9 @@
+<div class="flex items-center space-x-4 mb-8">
+    <img class="" style="width: 50px; height: 50px; margin-right: 10px" src="{{asset('images/logo.png')}}">
+
+</div>
+<div class="space-x-4 ">
+    SI-Akademik
+</div>
+
+
