30b5d4e85f7c117f999fa3004c925e859203c812
Dewantara <drawww111@gmail.com>
2025-10-25 00:04:34 +0900

    Feat : Adding Action Group

--- app/Filament/Resources/SuratKeputusans/Pages/ViewSuratKeputusan.php
+++ app/Filament/Resources/SuratKeputusans/Pages/ViewSuratKeputusan.php
@@ -4,9 +4,11 @@ namespace App\Filament\Resources\SuratKeputusans\Pages;
 
 use App\Filament\Resources\SuratKeputusans\SuratKeputusanResource;
 use Filament\Actions\Action;
+use Filament\Actions\ActionGroup;
 use Filament\Actions\EditAction;
 use Filament\Resources\Pages\ViewRecord;
 use Filament\Schemas\Schema;
+use Filament\Support\Enums\Width;
 
 class ViewSuratKeputusan extends ViewRecord
 {
@@ -19,11 +21,19 @@ class ViewSuratKeputusan extends ViewRecord
 
         return [
             EditAction::make(),
-            Action::make('Print')
-                ->icon('heroicon-o-printer')
-                ->color('success')
-                ->hidden(auth()->user()->role != 'admin')
-                ->url(fn () => route('skPDF', $dataId))
+            ActionGroup::make([
+                Action::make('Print')
+                    ->icon('heroicon-o-printer')
+                    ->color('success')
+                    ->hidden(auth()->user()->role != 'admin')
+                    ->url(fn () => route('skPDF', $dataId)),
+                Action::make('Print Dengan Tanda Tangan')
+                    ->icon('heroicon-o-printer')
+                    ->color('success')
+                    ->hidden(auth()->user()->role != 'admin')
+                    ->url(fn () => route('skttdPDF', $dataId))
+            ])
+            ->dropdownWidth(Width::Large),
         ];
     }
 
