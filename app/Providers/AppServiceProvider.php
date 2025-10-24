a1fde7a1939f90a9ad24273dfc9c241c75748f56
Dewantara <drawww111@gmail.com>
2025-10-25 00:05:01 +0900

    Fix: Ngrok Css not loaded in Ngrok

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use App\Http\Responses\LogoutResponse;
 use Carbon\Carbon;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 use Filament\Auth\Http\Responses\Contracts\LogoutResponse as LogoutResponseContract;
 
@@ -27,5 +28,8 @@ class AppServiceProvider extends ServiceProvider
     {
         //
         Carbon::setLocale('id');
+        if (env('APP_ENV') !== 'local' || str_contains(request()->getHost(), '.ngrok.io') || str_contains(request()->getHost(), '.ngrok-free.app')) { // Check for ngrok domain
+            URL::forceScheme('https');
+        }
     }
 }
