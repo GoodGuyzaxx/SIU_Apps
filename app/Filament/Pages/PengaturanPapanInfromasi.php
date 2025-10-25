f8de62ed4de7f3ad2e0f2691837c20ff60a51b60
Dewantara <drawww111@gmail.com>
2025-10-25 19:28:04 +0900

    Fix : error

--- app/Filament/Pages/PengaturanPapanInfromasi.php
+++ app/Filament/Pages/PengaturanPapanInfromasi.php
@@ -19,12 +19,12 @@ use UnitEnum;
 
 class PengaturanPapanInfromasi extends Page
 {
-    protected static string | BackedEnum | null $navigationIcon = 'heroicon-o-globe-alt';
-    protected static ?string $navigationLabel = 'Papan Informasi Digital';
+    protected static string|BackedEnum|null $navigationIcon = "heroicon-o-globe-alt";
+    protected static ?string $navigationLabel = "Papan Informasi Digital";
 
-    protected string $view = 'filament.pages.papan-informasi';
+    protected string $view = "filament.pages.papan-informasi";
 
-    protected static string | UnitEnum | null $navigationGroup = "Display & TV";
+    protected static string|UnitEnum|null $navigationGroup = "Display & TV";
     protected static ?int $navigationSort = 10;
 
     public ?array $data = [];
@@ -32,133 +32,138 @@ class PengaturanPapanInfromasi extends Page
     protected function getHeaderActions(): array
     {
         return [
-          Action::make('papan-informasi')
-            ->label('Papan Informasi')
-            ->url(fn () => url(route('info')))
-            ->openUrlInNewTab()
+            Action::make("papan-informasi")
+                ->label("Papan Informasi")
+                ->url(fn() => url(route("info")))
+                ->openUrlInNewTab(),
         ];
     }
 
-//    SCHEME
-\
-    public function informasiForm(Schema $schema): Schema{
-        return $schema->components([
-           Section::make('Papan Infromasi Digital')
-           ->schema([
-               TextInput::make('yt_url')
-                   ->label('Masukan URL Youtube')
-                   ->suffixIcon('heroicon-o-globe-alt')
-                   ->url(),
-           ]),
-            Section::make('Informasi Jadwal Proposal')
-                ->description('Masukan rentang Tanggal jadwal proposal yang akan ditampilkan')
-                ->schema([
-                    Grid::make(2)
-                        ->schema([
-                            DatePicker::make('tanggal_awal_proposal')
-                                ->label('Rentang tanggal awal proposal'),
-                            DatePicker::make('tanggal_akhir_proposal')
-                                ->label('Rentang tanggal akhir proposal'),
-                        ])
+    //    SCHEME
+    public function informasiForm(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                Section::make("Papan Infromasi Digital")->schema([
+                    TextInput::make("yt_url")
+                        ->label("Masukan URL Youtube")
+                        ->suffixIcon("heroicon-o-globe-alt")
+                        ->url(),
                 ]),
-            Section::make('Informasi Jadwal Skripsi')
-                ->description('Masukan rentan Tanggal jadwal Skripsi yang akan ditampilkan')
-                ->schema([
-                    Grid::make(2)
-                        ->schema([
-                            DatePicker::make('tanggal_awal_skripsi')
-                                ->label('Rentang tanggal awal skripsi'),
-                            DatePicker::make('tanggal_akhir_skripsi')
-                                ->label('Rentang tanggal akhir skripsi'),
-                        ])
-                ]),
-            Section::make('Informasi Pengajuan Judul')
-                ->description('Masukan rentan Tanggal Pengajuan Judual yang akan ditampilkan')
-                ->schema([
-                    Grid::make(1)
-                        ->schema([
-                            DatePicker::make('tanggal_pengajuan')
-                                ->label('Rentang dalam Bulan')
-                                ->format('m')
-                                ->extraInputAttributes(['type' => 'month']),
-                        ])
-                ]),
-        ])->statePath('data');
+                Section::make("Informasi Jadwal Proposal")
+                    ->description(
+                        "Masukan rentang Tanggal jadwal proposal yang akan ditampilkan",
+                    )
+                    ->schema([
+                        Grid::make(2)->schema([
+                            DatePicker::make("tanggal_awal_proposal")->label(
+                                "Rentang tanggal awal proposal",
+                            ),
+                            DatePicker::make("tanggal_akhir_proposal")->label(
+                                "Rentang tanggal akhir proposal",
+                            ),
+                        ]),
+                    ]),
+                Section::make("Informasi Jadwal Skripsi")
+                    ->description(
+                        "Masukan rentan Tanggal jadwal Skripsi yang akan ditampilkan",
+                    )
+                    ->schema([
+                        Grid::make(2)->schema([
+                            DatePicker::make("tanggal_awal_skripsi")->label(
+                                "Rentang tanggal awal skripsi",
+                            ),
+                            DatePicker::make("tanggal_akhir_skripsi")->label(
+                                "Rentang tanggal akhir skripsi",
+                            ),
+                        ]),
+                    ]),
+                Section::make("Informasi Pengajuan Judul")
+                    ->description(
+                        "Masukan rentan Tanggal Pengajuan Judual yang akan ditampilkan",
+                    )
+                    ->schema([
+                        Grid::make(1)->schema([
+                            DatePicker::make("tanggal_pengajuan")
+                                ->label("Rentang dalam Bulan")
+                                ->format("m")
+                                ->extraInputAttributes(["type" => "month"]),
+                        ]),
+                    ]),
+            ])
+            ->statePath("data");
     }
 
-//     Function Save Data....
+    //     Function Save Data....
     public function saveData()
     {
         $dataForm = $this->informasiForm->getState();
-//        dd($dataForm);
+        //        dd($dataForm);
 
-        $dataProposal = Undangan::with( 'judul')
-            ->whereHas('judul', function ($query) {
-                $query->where('jenis', 'proposal');
+        $dataProposal = Undangan::with("judul")
+            ->whereHas("judul", function ($query) {
+                $query->where("jenis", "proposal");
             })
-            ->whereBetween('tanggal_hari', [
-                $dataForm['tanggal_awal_proposal'],
-                $dataForm['tanggal_akhir_proposal'],
+            ->whereBetween("tanggal_hari", [
+                $dataForm["tanggal_awal_proposal"],
+                $dataForm["tanggal_akhir_proposal"],
             ])
             ->get()
             ->map(function ($item) {
                 return [
-                    'nama'  => $item->judul->mahasiswa->nama ?? '-',
-                    'npm'   => $item->judul->mahasiswa->npm ?? '-',
-                    'judul' => $item->judul->judul ?? '-',
-                    'tanggal_hari' => $item->tanggal_hari ?? '-',
-                    'waktu' => $item->waktu ?? '-'
+                    "nama" => $item->judul->mahasiswa->nama ?? "-",
+                    "npm" => $item->judul->mahasiswa->npm ?? "-",
+                    "judul" => $item->judul->judul ?? "-",
+                    "tanggal_hari" => $item->tanggal_hari ?? "-",
+                    "waktu" => $item->waktu ?? "-",
                 ];
             });
 
-        $dataUjianAkhir = Undangan::with( 'judul')
-            ->whereHas('judul', function ($query) {
-                $query->where('jenis', 'skripsi');
+        $dataUjianAkhir = Undangan::with("judul")
+            ->whereHas("judul", function ($query) {
+                $query->where("jenis", "skripsi");
             })
-            ->whereBetween('tanggal_hari', [
-                $dataForm['tanggal_awal_skripsi'],
-                $dataForm['tanggal_akhir_skripsi'],
+            ->whereBetween("tanggal_hari", [
+                $dataForm["tanggal_awal_skripsi"],
+                $dataForm["tanggal_akhir_skripsi"],
             ])
             ->get()
             ->map(function ($item) {
                 return [
-                    'nama'  => $item->judul->mahasiswa->nama ?? '-',
-                    'npm'   => $item->judul->mahasiswa->npm ?? '-',
-                    'judul' => $item->judul->judul ?? '-',
-                    'tanggal_hari' => $item->tanggal_hari ?? '-',
-                    'waktu' => $item->waktu ?? '-'
+                    "nama" => $item->judul->mahasiswa->nama ?? "-",
+                    "npm" => $item->judul->mahasiswa->npm ?? "-",
+                    "judul" => $item->judul->judul ?? "-",
+                    "tanggal_hari" => $item->tanggal_hari ?? "-",
+                    "waktu" => $item->waktu ?? "-",
                 ];
             });
 
-
-        $dataUsulan = UsulanJudul::with('mahasiswa')
-            ->whereMonth('created_at', $dataForm['tanggal_pengajuan'])
+        $dataUsulan = UsulanJudul::with("mahasiswa")
+            ->whereMonth("created_at", $dataForm["tanggal_pengajuan"])
             ->get()
             ->map(function ($usulan) {
                 return [
-                    'nama' => $usulan->mahasiswa->nama,
-                    'npm' => $usulan->mahasiswa->npm,
-                    'status' => $usulan->status,
+                    "nama" => $usulan->mahasiswa->nama,
+                    "npm" => $usulan->mahasiswa->npm,
+                    "status" => $usulan->status,
                 ];
             });
 
+        //        dd($dataUsulan);
 
-//        dd($dataUsulan);
-
-        $papanInfromasi = PapanInformasi::where('id', 1)->first();
+        $papanInfromasi = PapanInformasi::where("id", 1)->first();
         $papanInfromasi->update([
-            'yt_url' => $dataForm['yt_url'],
-            'jadwal_proposal' => $dataProposal,
-            'jadwal_skripsi' => $dataUjianAkhir,
-            'pengajuan_judul' => $dataUsulan
+            "yt_url" => $dataForm["yt_url"],
+            "jadwal_proposal" => $dataProposal,
+            "jadwal_skripsi" => $dataUjianAkhir,
+            "pengajuan_judul" => $dataUsulan,
         ]);
 
         Notification::make()
             ->title("Data Berhasil Disimpan")
             ->success()
             ->send();
-
     }
 
-//      Open Infromasi
+    //      Open Infromasi
 }
