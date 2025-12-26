58fb43e59ebbde5b21b35246942ba92eb47afdd1
Dewantara <46391302+GoodGuyzaxx@users.noreply.github.com>
2025-12-27 01:29:57 +0900

    change url route

--- routes/web.php
+++ routes/web.php
@@ -7,14 +7,14 @@ Route::get('/', function () {
     return redirect('/user');
 });
 
-Route::get('/admin/undangan/pdf/{id}',[\App\Http\Controllers\UndanganController::class, 'getPDF'] )->name('undangan.pdf');
-Route::get('/admin/undangan/pdf/ttd/{id}',[\App\Http\Controllers\UndanganController::class, 'getTtdPDF'] )->name('undangan.ttd.pdf');
+Route::get('/dokumen/undangan/pdf/{id}',[\App\Http\Controllers\UndanganController::class, 'getPDF'] )->name('undangan.pdf');
+Route::get('/dokumen/undangan/pdf/ttd/{id}',[\App\Http\Controllers\UndanganController::class, 'getTtdPDF'] )->name('undangan.ttd.pdf');
 
 Route::get('info', \App\Livewire\PapanInfo::class)->name('info');
 
-Route::get('/admin/sk/pdf/{id}', [UndanganController::class, 'getSkPDF'] )->name('skPDF');
+Route::get('/dokumen/sk/pdf/{id}', [UndanganController::class, 'getSkPDF'] )->name('skPDF');
 
-Route::get('/admin/sk/pdf/ttd/{id}', [UndanganController::class, 'getTtdSkPDF'] )->name('skttdPDF');
+Route::get('/dokumen/sk/pdf/ttd/{id}', [UndanganController::class, 'getTtdSkPDF'] )->name('skttdPDF');
 
 Route::get('/admin/berita-acara/pdf/{id}/{jenis}/{waktu}', [UndanganController::class, 'getBeritaAcaraPdf'] )->name('beritaPDF');
 
